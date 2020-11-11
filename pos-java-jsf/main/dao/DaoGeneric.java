7308df11bf4d92cdf77165098f45a67a15d60924
junior <juniorpropaganda@gmail.com>
2020-11-10 21:36:12 -0300

    projeto esta compilando JSF

---
+++ pos-java-jsf/main/dao/DaoGeneric.java
@@ -0,0 +1,79 @@
+package dao;
+
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+import pos_java_maven_hibernate.HibernateUtil;
+
+public class DaoGeneric<E> {
+
+	private EntityManager entityManager = HibernateUtil.geEntityManager();
+
+	public void salvar(E entidade) {
+		EntityTransaction transaction = entityManager.getTransaction();
+		transaction.begin();
+		entityManager.persist(entidade);
+		transaction.commit();
+	}
+
+	public E updateMerge(E entidade) { // salva ou atualiza
+		EntityTransaction transaction = entityManager.getTransaction();
+		transaction.begin();
+		E entidadeSalva = entityManager.merge(entidade);
+		transaction.commit();
+
+		return entidadeSalva;
+	}
+
+	public E pesquisar(E entidade) {
+		Object id = HibernateUtil.getPrimaryKey(entidade);
+
+		E e = (E) entityManager.find(entidade.getClass(), id);
+
+		return e;
+	}
+
+	public E pesquisar(Long id, Class<E> entidade) {
+		E e = (E) entityManager.find(entidade, id);
+		return e;
+	}
+
+	public void deletarPorId(E entidade) {
+		Object id = HibernateUtil.getPrimaryKey(entidade);
+		EntityTransaction transaction = entityManager.getTransaction();
+		transaction.begin();
+
+		entityManager
+				.createNativeQuery(
+						"delete from " + entidade.getClass().getSimpleName().toLowerCase() + " where id =" + id)
+				.executeUpdate(); // faz o delete
+		transaction.commit();// grava a alteração no banco
+	}
+
+	
+	
+	//Persistência em java
+	//unidade 2
+	//video PJ-UN02 - 12
+	
+	
+	public List<E> listar(Class<E> entidade) {
+		EntityTransaction transaction = entityManager.getTransaction();
+		transaction.begin();
+		
+		List<E> lista = entityManager.createQuery("from " + entidade.getName()).getResultList();
+		
+		transaction.commit();
+		
+		return lista;
+	}	
+
+	
+
+
+	
+	public EntityManager getEntityManager( ) {
+		return entityManager;
+	}
+
+}
